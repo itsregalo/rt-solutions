cecc5ae5d373c5bcd15d4e1b60cf465995f579cc
retech <itsregalo047@gmail.com>
2022-07-12 10:08:10 +0300

    fixed base

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', Index, name='index'),
+    path('services/', Services, name='services'),
+    path('about/', About, name='about'),
+    path('contact/', Contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def Index(request):
     return render(request, 'index.html')
+
+def Services(request):
+    return render(request, 'services.html')
+
+def About(request):
+    return render(request, 'about-us.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
